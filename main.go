dc891daf69a29d8d60fe9660be8ba0552b97e0bd
Shota Omori <omori@mobcast.jp>
2017-02-05 10:32:51 +0000

    commit

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"crypto/x509"
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("vim-go2")
+	//url := "https://socialnews.rakuten.co.jp/"
+	url := "https://github.com"
+
+	response, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	_, _ = confirmCertificate(response.TLS.PeerCertificates)
+
+}
+
+func confirmCertificate(certificates []*x509.Certificate) (string, error) {
+	for _, v := range certificates {
+		fmt.Println(v.MaxPathLen, v.OCSPServer, v.IsCA, v.NotBefore, v.NotAfter)
+		fmt.Println(v.Subject.CommonName)
+	}
+
+	return "", nil
 }
